10dad4975eed4375ea387181d35ad4e81f56af57
mohsen golab <golab.mohsen@gmail.com>
2022-08-15 13:17:36 +0430

    UpdateService

---
+++ ServiceDns/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace ServiceDns
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(191, 131);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(337, 32);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Location = new System.Drawing.Point(180, 211);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 37);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "CreateDns";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Maroon;
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(325, 211);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(113, 37);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "RemoveDns";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(59, 136);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "DomainName:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(620, 380);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "ServiceDns";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ServiceDns/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ServiceDns
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Trim().Length > 1)
+            {
+                InitialSessionState iss = InitialSessionState.CreateDefault2();
+
+              
+                var shell = PowerShell.Create(iss);
+
+
+
+                shell.Commands.AddScript(@"Add-DnsServerPrimaryZone -Name " + textBox1.Text.Trim() + " -ZoneFile " + textBox1.Text.Trim() + ".dns");
+                shell.Commands.AddScript("Add-DnsServerResourceRecordA -Name \"@\" -ZoneName \"" + textBox1.Text.Trim() + "\" -AllowUpdateAny -IPv4Address \"37.156.145.183\" -TimeToLive 01:00:00");
+                shell.Commands.AddScript("Add-DnsServerResourceRecordA -Name \"www\" -ZoneName \"" + textBox1.Text.Trim() + "\" -AllowUpdateAny -IPv4Address \"37.156.145.183\" -TimeToLive 01:00:00");
+                
+                try
+                {
+                    var results = shell.Invoke();
+
+                  
+                    if (results.Count > 0)
+                    {
+                      
+                        var builder = new System.Text.StringBuilder();
+
+                        foreach (var psObject in results)
+                        {
+                          
+                            builder.Append(psObject.BaseObject.ToString() + "\r\n");
+                        }
+                    }
+                }
+                catch (Exception ex) { }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            //textBox1.Text = str;
+            if (textBox1.Text.Length > 1)
+            {
+                InitialSessionState iss = InitialSessionState.CreateDefault2();
+
+                var shell = PowerShell.Create(iss);
+
+                shell.Commands.AddScript("Remove-DnsServerZone \"" + textBox1.Text.Trim() + "\" -PassThru -Force");
+                try
+                {
+                    var results = shell.Invoke();
+
+                    if (results.Count > 0)
+                    {
+                        var builder = new System.Text.StringBuilder();
+
+                        foreach (var psObject in results)
+                        {
+                            builder.Append(psObject.BaseObject.ToString() + "\r\n");
+                        }
+                    }
+                }
+                catch (Exception ex) { }
+            }
+
+        }
+    }
+}
